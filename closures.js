d8420b0bc95cb09159a6872423c31fc2c97542b0
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-15 16:46:46 -0500

    fixed last problem closures

--- closures.js
+++ closures.js
@@ -206,10 +206,13 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    for (var i = 0; i <= 5; i++) {
+      (function(i) {
+      setTimeout(function() {
+        console.log(i);
+      }, i * 1000);
+
+    } (i));
   }
 }
 timeOutCounter();
